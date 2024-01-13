9d0c770ed50ba864c5bfda91783bef65dd9b369e
carghai <carghai74@gmail.com>
2024-01-13 10:11:54 -0500

    fix

--- src/main/java/team1403/robot/Robot.java
+++ src/main/java/team1403/robot/Robot.java
@@ -30,7 +30,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-    m_VisionCommand = new PhotonVisionCommand(m_robotContainer.getLimelight());
+    m_VisionCommand = new PhotonVisionCommand(m_robotContainer.getLimelight(), m_robotContainer.getSwerveSubsystem());
   }
 
   /**
