7e9e8c7e6ce3b8b9bcc1428c54bb927adbd4b2e2
tinyrice01 <widee135@gmail.com>
2024-01-17 15:34:20 -0500

    added motors, method to stop, set speed, and to make sure the motors are at the same speed

--- src/main/java/team1403/robot/subsystems/Shooter.java
+++ src/main/java/team1403/robot/subsystems/Shooter.java
@@ -7,27 +7,57 @@ import com.revrobotics.SparkRelativeEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import team1403.lib.core.CougarLibInjectedParameters;
 import team1403.lib.device.wpi.CougarSparkMax;
-import team1403.lib.device.wpi.WpiLimitSwitch;
 import team1403.robot.Constants;
 
 public class Shooter extends SubsystemBase {
 
-  private CougarSparkMax m_shooterMotor;
-  private WpiLimitSwitch m_shooterLimitSwitch;
-
+  private CougarSparkMax m_shooterMotorTop;
+  private CougarSparkMax m_shooterMotorBottom;
+  private double lastSpeed = 0;
 
   public Shooter(CougarLibInjectedParameters injectedParameters) {
-    m_shooterMotor = CougarSparkMax.makeBrushless(
-      "Shooter Motor", Constants.CanBus.hangerMotor, SparkRelativeEncoder.Type.kHallSensor);
+    m_shooterMotorTop = CougarSparkMax.makeBrushless(
+      "Top Shooter Motor", Constants.CanBus.shooterMotorTop, SparkRelativeEncoder.Type.kHallSensor);
+    m_shooterMotorBottom = CougarSparkMax.makeBrushless(
+      "Bottom Shooter Motor", Constants.CanBus.shooterMotorBottom, SparkRelativeEncoder.Type.kHallSensor);
 
-    m_shooterLimitSwitch = new WpiLimitSwitch(
-      "Shooter Limit Switch", Constants.RioPorts.shooterLimitSwitchPort);
   }
+  public boolean ready() {
+    if (lastSpeed == m_shooterMotorTop.getEncoder().getVelocity() && lastSpeed == m_shooterMotorBottom.getEncoder().getVelocity()) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  public boolean speedIsEqual() {
+
+    if (m_shooterMotorTop.getEncoder().getVelocity() == m_shooterMotorBottom.getEncoder().getVelocity()) {
+
+        return true;
+
+    } else {
+
+        return false;
+
+    }
+
+  }
+
+public void stop(){
+  m_shooterMotorTop.setSpeed(0);
+  m_shooterMotorBottom.setSpeed(0);
+}
 
   public void setShooterSpeed(double speed) {
-    m_shooterMotor.set(speed);
+    lastSpeed = speed;
+    m_shooterMotorTop.set(speed);
+    m_shooterMotorBottom.set(speed);
   }
   public void periodic() {
-    Logger.recordOutput("Shooter Temp", m_shooterMotor.getMotorTemperature());
+    Logger.recordOutput("Shooter Temp", m_shooterMotorTop.getMotorTemperature());
+    Logger.recordOutput("Shooter Temp", m_shooterMotorBottom.getMotorTemperature());
+
   }
 }
