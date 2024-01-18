cd1b23a31d313cb23b86241f86479af7c1b3dd7d
Brian Chen <brian.chen10204@gmail.com>
2024-01-18 15:59:48 -0500

    fixed RunShooter and RunIntake command variables

--- src/main/java/team1403/robot/commands/RunIntake.java
+++ src/main/java/team1403/robot/commands/RunIntake.java
@@ -4,26 +4,26 @@ import edu.wpi.first.wpilibj2.command.Command;
 import team1403.robot.subsystems.IntakeAndShooter;
 
 public class RunIntake extends Command {
-    private IntakeAndShooter m_intakeAndShooter;
-    private double m_intakeAndShooterSpeed;
+    private IntakeAndShooter m_intake;
+    private double m_intakeSpeed;
 
-    public RunIntake(IntakeAndShooter intakeAndShooter, double intakeSpeed) {
-        m_intakeAndShooter = intakeAndShooter;
-        m_intakeAndShooterSpeed = intakeSpeed;
+    public RunIntake(IntakeAndShooter intake, double intakeSpeed) {
+        m_intake = intake;
+        m_intakeSpeed = intakeSpeed;
     }
 
     @Override
     public void initialize() {
-        m_intakeAndShooter.setShooterSpeed(m_intakeAndShooterSpeed);
+        m_intake.setIntakeSpeed(m_intakeSpeed);
     }
 
     @Override
     public void execute() {
-        //intake note
+        //intakes note
     }
 
     @Override
     public boolean isFinished() {
-        return m_intakeAndShooter.intakeReady();
+        return m_intake.intakeReady();
     }
 }
