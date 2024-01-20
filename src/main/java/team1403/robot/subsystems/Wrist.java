114694f7250211db61bc6fcae507e2c91290fe87
Brian Chen <brian.chen10204@gmail.com>
2024-01-20 13:07:52 -0500

    removed cougarlibinjectedparameters from wrist subsystem

--- src/main/java/team1403/robot/subsystems/Wrist.java
+++ src/main/java/team1403/robot/subsystems/Wrist.java
@@ -5,8 +5,6 @@ import org.littletonrobotics.junction.Logger;
 import com.revrobotics.SparkRelativeEncoder;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-import team1403.lib.core.CougarLibInjectedParameters;
 import team1403.lib.device.wpi.CougarSparkMax;
 import team1403.robot.Constants;
 
@@ -14,7 +12,7 @@ public class Wrist extends SubsystemBase {
     private CougarSparkMax m_wristMotor;
     private double lastSpeed = 0;
 
-public Wrist(CougarLibInjectedParameters injectedParameters) {
+public Wrist() {
     m_wristMotor = CougarSparkMax.makeBrushless("Wrist Motor", Constants.CanBus.wristMotor, SparkRelativeEncoder.Type.kHallSensor);
   }
 
