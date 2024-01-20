301648e00bff1e2f3e1c9f3bc864a0d7b9b48065
Shreyas Kakkar <92545590+ShreyasK06@users.noreply.github.com>
2024-01-20 10:29:28 -0500

    remove field layout constant

--- src/main/java/team1403/robot/Constants.java
+++ src/main/java/team1403/robot/Constants.java
@@ -107,50 +107,6 @@ public class Constants {
         kMaxAngularAccelerationRadiansPerSecondSquared);
   }
 
-  public static class Vision {
-    public static AprilTagFieldLayout fieldLayout = new AprilTagFieldLayout(Arrays.asList(
-      new AprilTag(1,   (new Pose3d(
-        Units.inchesToMeters(610.77),
-        Units.inchesToMeters(42.19),
-        Units.inchesToMeters(18.22),
-        new Rotation3d(0.0, 0.0, Math.PI)))),
-      new AprilTag(2, new Pose3d(
-        Units.inchesToMeters(610.77),
-        Units.inchesToMeters(108.19),
-        Units.inchesToMeters(18.22),
-        new Rotation3d(0.0, 0.0, Math.PI))),
-      new AprilTag(3,new Pose3d(
-        Units.inchesToMeters(610.77),
-        Units.inchesToMeters(174.19), // FIRST's diagram has a typo (it says 147.19)
-        Units.inchesToMeters(18.22),
-        new Rotation3d(0.0, 0.0, Math.PI))),
-      new AprilTag(4, new Pose3d(
-        Units.inchesToMeters(636.96),
-        Units.inchesToMeters(265.74),
-        Units.inchesToMeters(27.38),
-        new Rotation3d(0.0, 0.0, Math.PI))), 
-      new AprilTag(5, new Pose3d(
-        Units.inchesToMeters(14.25),
-        Units.inchesToMeters(265.74),
-        Units.inchesToMeters(27.38),
-        new Rotation3d())), 
-      new AprilTag(6, new Pose3d(
-        Units.inchesToMeters(610.77),
-        Units.inchesToMeters(174.19), // FIRST's diagram has a typo (it says 147.19)
-        Units.inchesToMeters(18.22),
-        new Rotation3d(0.0, 0.0, Math.PI))),
-      new AprilTag(7, new Pose3d(
-        Units.inchesToMeters(610.77),
-        Units.inchesToMeters(108.19),
-        Units.inchesToMeters(18.22),
-        new Rotation3d(0.0, 0.0, Math.PI))),  
-      new AprilTag(8, new Pose3d(
-        Units.inchesToMeters(40.45),
-        Units.inchesToMeters(42.19),
-        Units.inchesToMeters(18.22),
-        new Rotation3d()))), Units.inchesToMeters(651.25), Units.inchesToMeters(315.5));
-  }
-
   /**
    * Configures the CAN bus. These are grouped together
    * rather than by subsystem to more easily detect conflict
