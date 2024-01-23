6b71252077869e4f1dd9b9fe794f263b7645b1be
tinyrice01 <widee135@gmail.com>
2024-01-23 16:15:53 -0500

    updated run shooter code

--- src/main/java/team1403/robot/commands/RunShooter.java
+++ src/main/java/team1403/robot/commands/RunShooter.java
@@ -2,14 +2,22 @@ package team1403.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import team1403.robot.subsystems.Shooter;
+import team1403.robot.subsystems.Intake;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 public class RunShooter extends Command {
     private Shooter m_shooter;
+    private Intake m_intake;
     private double m_shooterSpeed;
+    private double m_intakeSpeed;
+    private DigitalInput m_shooterPhotogate;
 
-    public RunShooter(Shooter shooter, double shooterSpeed) {
+    public RunShooter(Shooter shooter, double shooterSpeed, Intake intake, double intakeSpeed, DigitalInput shooterPhotogate) {
         m_shooter = shooter;
+        m_intake = intake;
         m_shooterSpeed = shooterSpeed;
+        m_intakeSpeed = intakeSpeed;
+        m_shooterPhotogate = shooterPhotogate;
     }
 
     @Override
@@ -19,11 +27,16 @@ public class RunShooter extends Command {
 
     @Override
     public void execute() {
-        //shoots note
+        m_intake.setIntakeSpeed(m_intakeSpeed);
     }
 
     @Override
     public boolean isFinished() {
-        return m_shooter.shooterReady();
+        if (m_shooterPhotogate.get() == true) {
+            m_intake.stop();
+            m_shooter.stop();
+            return true;
+        }
+        return false;
     }
 }
