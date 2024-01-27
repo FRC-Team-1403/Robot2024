14c8f30ff11efe435fbd76231058f773428194b9
tinyrice01 <widee135@gmail.com>
2024-01-27 13:56:47 -0500

    added talons to shooter
    Co-authored-by: Brian Chen <spraky3353@users.noreply.github.com>

--- src/main/java/team1403/robot/subsystems/Shooter.java
+++ src/main/java/team1403/robot/subsystems/Shooter.java
@@ -3,31 +3,24 @@ package team1403.robot.subsystems;
 
 import org.littletonrobotics.junction.Logger;
 
-
 import com.revrobotics.SparkRelativeEncoder;
-
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.DigitalInput;
-import team1403.lib.device.wpi.CougarSparkMax;
 import team1403.robot.Constants;
+import com.ctre.phoenix6.hardware.TalonFX;
 
 
 public class Shooter extends SubsystemBase {
-
-
- private CougarSparkMax m_motorTop;
- private CougarSparkMax m_motorBottom;
  private DigitalInput m_shooterPhotogate;
  private double lastSpeed = 0;
-
+ private TalonFX m_falconTop;
+ private TalonFX m_falconBottom;
 
  public Shooter() {
-   m_motorTop = CougarSparkMax.makeBrushless(
-     "Top Shooter Motor", Constants.CanBus.shooterMotorTop, SparkRelativeEncoder.Type.kHallSensor);
-   m_motorBottom = CougarSparkMax.makeBrushless(
-     "Bottom Shooter Motor", Constants.CanBus.shooterMotorBottom, SparkRelativeEncoder.Type.kHallSensor);
-   m_shooterPhotogate = new DigitalInput(Constants.RioPorts.shooterPhotogate);
+  m_falconTop = new TalonFX(Constants.CanBus.shooterMotorTop);
+  m_falconBottom = new TalonFX(Constants.CanBus.shooterMotorBottom);
+  m_shooterPhotogate = new DigitalInput(Constants.RioPorts.shooterPhotogate);
  }
 
 public boolean shooterPhotogate() {
@@ -35,7 +28,7 @@ public boolean shooterPhotogate() {
 }
 
  public boolean shooterReady() {
-   if (lastSpeed == Math.abs(m_motorTop.getEncoder().getVelocity()) && lastSpeed == Math.abs(m_motorBottom.getEncoder().getVelocity())) {
+   if (lastSpeed == Math.abs(m_falconTop.get()) && lastSpeed == Math.abs(m_falconBottom.get())) {
      return true;
    }
    else {
@@ -45,7 +38,7 @@ public boolean shooterPhotogate() {
 
 
  public boolean speedIsEqual() {
-   if (Math.abs(m_motorTop.getEncoder().getVelocity()) == Math.abs(m_motorBottom.getEncoder().getVelocity())) {
+   if (Math.abs(m_falconTop.get()) == Math.abs(m_falconBottom.get())) {
        return true;
    } else {
        return false;
@@ -54,23 +47,23 @@ public boolean shooterPhotogate() {
 
 
  public void stop() {
-   m_motorTop.setSpeed(0);
-   m_motorBottom.setSpeed(0);
+   m_falconTop.set(0);
+   m_falconBottom.set(0);
  }
 
 
  public void setShooterSpeed(double speed) {
    lastSpeed = speed;
-   m_motorTop.set(speed);
-   m_motorBottom.set(-(speed));
+   m_falconTop.set(speed);
+   m_falconBottom.set(-(speed));
    //if there is an error when testing (note doesn't shoot) try changing the direction of the motor
  }
 
 
  public void periodic() {
-   Logger.recordOutput("Shooter Top Motor Temp", m_motorTop.getMotorTemperature());
-   Logger.recordOutput("Shooter Bottom Motor Temp", m_motorBottom.getMotorTemperature());
-   Logger.recordOutput("Shooter Top Motor RPM", m_motorTop.getVoltageCompensationNominalVoltage());
-   Logger.recordOutput("Shooter Bottom Motor RPM", m_motorBottom.getVoltageCompensationNominalVoltage());
+  //  Logger.recordOutput("Shooter Top Motor Temp", m_falconTop.getMotorTemperature());
+  //  Logger.recordOutput("Shooter Bottom Motor Temp", m_falconBottom.getMotorTemperature());
+   Logger.recordOutput("Shooter Top Motor RPM", m_falconTop.get());
+   Logger.recordOutput("Shooter Bottom Motor RPM", m_falconBottom.get());
  }
 }
