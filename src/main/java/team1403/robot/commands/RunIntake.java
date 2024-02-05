816e914dad04ac2ef24d96db92dbf40a3d33d4d9
anishka-k <Anishka.kolli@gmail.com>
2024-02-05 17:49:50 -0500

    making the intake and shooter one file
    merged the intake and shooter subsystem files into one file, and put their commands on one file.

--- src/main/java/team1403/robot/commands/RunIntake.java
+++ src/main/java/team1403/robot/commands/RunIntake.java
@@ -1,44 +1,44 @@
-package team1403.robot.commands;
+// package team1403.robot.commands;
 
 
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj2.command.Command;
-import team1403.robot.Constants;
-import team1403.robot.subsystems.Intake;
+// import edu.wpi.first.wpilibj.DigitalInput;
+// import edu.wpi.first.wpilibj2.command.Command;
+// import team1403.robot.Constants;
+// import team1403.robot.subsystems.IntakeAndShooter;
 
 
-public class RunIntake extends Command {
-   private Intake m_intake;
-   private double m_intakeSpeed;
-   private DigitalInput m_intakePhotogate1;
-   private DigitalInput m_Photogate2;
+// public class RunIntake extends Command {
+//    private IntakeAndShooter m_intake;
+//    private double m_intakeSpeed;
+//    private DigitalInput m_intakePhotogate1;
+//    private DigitalInput m_Photogate2;
    
 
-   public RunIntake(Intake intake, double intakeSpeed, DigitalInput intakePhotogate, DigitalInput Photogate2) {
-       m_intake = intake;
-       m_intakeSpeed = intakeSpeed;
-       m_intakePhotogate1 = intakePhotogate;
-       m_Photogate2 = Photogate2;
-   }
+//    public RunIntake(IntakeAndShooter intake, double intakeSpeed, DigitalInput intakePhotogate, DigitalInput Photogate2) {
+//        m_intake = intake;
+//        m_intakeSpeed = intakeSpeed;
+//        m_intakePhotogate1 = intakePhotogate;
+//        m_Photogate2 = Photogate2;
+//    }
 
-   @Override
-   public void initialize() {
-       m_intake.setIntakeSpeed(m_intakeSpeed);
-   }
+//    @Override
+//    public void initialize() {
+//        m_intake.setIntakeSpeed(m_intakeSpeed);
+//    }
 
-   //slows down intake motor when intakePhotogate1 is hit 
-   @Override
-   public void execute() {  
-        if(m_intakePhotogate1.get()){
-            m_intake.setIntakeSpeed(m_intakeSpeed / Constants.Arm.kDecreaseIntakeSpeed); 
-        } 
-   }
-   @Override
-   public boolean isFinished() {
-       if (m_Photogate2.get()) {
-         m_intake.stop();
-       }
-       return m_Photogate2.get();
-    }
+//    //slows down intake motor when intakePhotogate1 is hit 
+//    @Override
+//    public void execute() {  
+//         if(m_intakePhotogate1.get()){
+//             m_intake.setIntakeSpeed(m_intakeSpeed / Constants.Arm.kDecreaseIntakeSpeed); 
+//         } 
+//    }
+//    @Override
+//    public boolean isFinished() {
+//        if (m_Photogate2.get()) {
+//          m_intake.stop();
+//        }
+//        return m_Photogate2.get();
+//     }
 
-}
+// }
