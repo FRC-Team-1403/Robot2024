bfabb50ffb99f0327b3f0f42dfadeb1c50df9036
carghai <carghai74@gmail.com>
2024-02-09 12:32:26 -0500

    stuff

---
+++ src/test/java/datatablesTest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class datatablesTest {
+    Tables
+    @Test // marks this method as a test
+    void regressionTest() {
+     
+    }
+}
