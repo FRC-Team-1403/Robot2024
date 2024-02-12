b30e0fc4f8c16a13b19ad2f27e0c36e43af98020
carghai <carghai74@gmail.com>
2024-02-12 17:19:12 -0500

    needs work

--- src/test/java/datatablesTest.java
+++ src/test/java/datatablesTest.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -6,34 +8,34 @@ import team1403.robot.Datables.Tables;
 
 public class datatablesTest {
     Tables table;
+
     @BeforeEach
     void setup() {
-        ShooterValues[][] plot = new ShooterValues[800][800];
-        for (int i = 100; i < 600; i+=4) {
-            for (int j = 100; j < 600; j+=4) {
-                plot[i][j] = new ShooterValues((i + 1) * 10, (i + 1) * 10, (i + 1) * 10);
-            }
+        HashMap<Double, ShooterValues> plot = new HashMap<Double, ShooterValues>();
+        for (double j = 10; j < 60; j += .1) {
+            plot.put(j, new ShooterValues((j + 1) * 10, (j + 1) * 10, (j + 1) * 10));
         }
         table = new Tables(plot);
     }
+
     @Test // marks this method as a test
     void regressionTest() {
-        System.out.println("Low test: " + table.compute(50, 50));
-        System.out.println("High test: " + table.compute(700, 700));
+        System.out.println("Low test: " + table.get(8.1));
+        System.out.println("High test: " + table.get(80.1));
     }
 
     @Test // marks this method as a test
     void normalTest() {
-        System.out.println("Normal test: " + table.compute(400, 400));
+        System.out.println("Normal test: " + table.get(42.1));
     }
 
     @Test // marks this method as a test
     void zero() {
-        System.out.println("Zero test: " + table.compute(0, 0));
+        System.out.println("Zero test: " + table.get(0.1));
     }
 
     @Test // marks this method as a test
     void midPoint() {
-        System.out.println("Mod test: " + table.compute(399, 399));
+        System.out.println("Mod test: " + table.get(45));
     }
 }
