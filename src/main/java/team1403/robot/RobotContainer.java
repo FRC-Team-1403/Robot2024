e599fbf221e7d131abda42d63c9a99b91437d739
carghai <carghai74@gmail.com>
2024-02-29 10:03:52 -0500

    name command added

--- src/main/java/team1403/robot/RobotContainer.java
+++ src/main/java/team1403/robot/RobotContainer.java
@@ -72,6 +72,9 @@ public class RobotContainer {
     NamedCommands.registerCommand("First Piece",new IntakeShooterLoop(m_endeff, m_arm, m_wrist, m_led, () -> true, () -> false, () -> false, () -> false, () -> false,  () -> false,  () -> false,false));
     NamedCommands.registerCommand("Shoot",new IntakeShooterLoop(m_endeff, m_arm, m_wrist, m_led, () -> true, () -> false, () -> false, () -> false, () -> true,  () -> false,  () -> false,false));
     NamedCommands.registerCommand("Shoot Side",new IntakeShooterLoop(m_endeff, m_arm, m_wrist, m_led, () -> true, () -> false, () -> false, () -> false, () -> true,  () -> false,  () -> false,true));
+    NamedCommands.registerCommand("IntakeHold", new IntakeCommand(m_endeff, m_arm, m_wrist));
+    NamedCommands.registerCommand("ShootHold", new IntakeCommand(m_endeff, m_arm, m_wrist));
+
 
     m_combinedCommand = new IntakeShooterLoop(
       m_endeff, m_arm, 
