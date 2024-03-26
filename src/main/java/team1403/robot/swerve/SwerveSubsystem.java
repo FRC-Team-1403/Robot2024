ce19d9d4fb97e1a3d8ac396af0375f40c52f1ee0
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2024-03-25 21:48:20 -0400

    switch to ChassisSpeeds.discretize for translationalDriftCorrection

--- src/main/java/team1403/robot/swerve/SwerveSubsystem.java
+++ src/main/java/team1403/robot/swerve/SwerveSubsystem.java
@@ -378,28 +378,6 @@ public class SwerveSubsystem extends SubsystemBase {
     return m_navx2;
   }
 
-  /**
-   * Borrowed from 254:
-   * https://github.com/Team254/FRC-2022-Public/blob/b5da3c760b78d598b492e1cc51d8331c2ad50f6a/src/main/java/com/team254/lib/geometry/Pose2d.java
-   */
-  private static Twist2d log(final Pose2d transform) {
-    final double dtheta = transform.getRotation().getRadians();
-    final double half_dtheta = 0.5 * dtheta;
-    final double cos_minus_one = Math.cos(transform.getRotation().getRadians()) - 1.0;
-    double halftheta_by_tan_of_halfdtheta;
-    if (Math.abs(cos_minus_one) < 1E-9) {
-      halftheta_by_tan_of_halfdtheta = 1.0 - 1.0 / 12.0 * dtheta * dtheta;
-    } else {
-      halftheta_by_tan_of_halfdtheta =
-          -(half_dtheta * Math.sin(transform.getRotation().getRadians())) / cos_minus_one;
-    }
-    final Translation2d translation_part =
-        transform
-            .getTranslation()
-            .rotateBy(new Rotation2d(halftheta_by_tan_of_halfdtheta, -half_dtheta));
-    return new Twist2d(translation_part.getX(), translation_part.getY(), dtheta);
-  }
-
   private double prevTimeStep = 0;
 
   private ChassisSpeeds translationalDriftCorrection(ChassisSpeeds chassisSpeeds) {
@@ -413,16 +391,14 @@ public class SwerveSubsystem extends SubsystemBase {
     final double deltaTime = curTimeStep - prevTimeStep;
     prevTimeStep = curTimeStep;
     
+    return ChassisSpeeds.discretize(chassisSpeeds, deltaTime);
+  }
 
-    // The position of the bot one control loop in the future given the chassisspeed
-    Pose2d robotPoseVel = new Pose2d(chassisSpeeds.vxMetersPerSecond * deltaTime,
-        chassisSpeeds.vyMetersPerSecond * deltaTime,
-        new Rotation2d(chassisSpeeds.omegaRadiansPerSecond * deltaTime));
+  private ChassisSpeeds rotationalDriftCorrection(ChassisSpeeds speeds) {
 
-    Twist2d twistVel = log(robotPoseVel);
-    return new ChassisSpeeds(
-        twistVel.dx / deltaTime, twistVel.dy / deltaTime,
-        twistVel.dtheta / deltaTime);
+    //implement another algorithm for this later
+
+    return speeds;
   }
 
   private void updateSwerveModules() {
@@ -456,7 +432,7 @@ public class SwerveSubsystem extends SubsystemBase {
       xMode();
     } else {
       m_chassisSpeeds = translationalDriftCorrection(m_chassisSpeeds);
-      // m_chassisSpeeds = rotationalDriftCorrection(m_chassisSpeeds);
+      if (DriverStation.isTeleop()) m_chassisSpeeds = rotationalDriftCorrection(m_chassisSpeeds);
 
       m_states = Swerve.kDriveKinematics.toSwerveModuleStates(m_chassisSpeeds, m_offset);
       setModuleStates(m_states);
