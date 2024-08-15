eaa0f017eca22cba33693c1bcb9b3f11b011a62f
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2024-08-15 15:12:17 -0400

    fix comment

--- src/main/java/team1403/robot/swerve/AprilTagCamera.java
+++ src/main/java/team1403/robot/swerve/AprilTagCamera.java
@@ -8,15 +8,10 @@ import org.littletonrobotics.junction.Logger;
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.PhotonUtils;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.targeting.PhotonPipelineResult;
-import org.photonvision.targeting.PhotonTrackedTarget;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import team1403.robot.Constants;
