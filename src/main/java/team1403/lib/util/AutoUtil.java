d0a53097b8bfe26e39683d8ad59accaa772120fd
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2024-09-21 10:21:56 -0400

    use kAllianceSupplier in AutoUtil

--- src/main/java/team1403/lib/util/AutoUtil.java
+++ src/main/java/team1403/lib/util/AutoUtil.java
@@ -4,7 +4,6 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.PathPlannerPath;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -17,7 +16,7 @@ public class AutoUtil {
     PathPlannerPath path = PathPlannerPath.fromChoreoTrajectory(name);
     Command cmd = AutoBuilder.followPath(path);
     return Commands.sequence(Commands.runOnce(() -> {
-        if(DriverStation.getAlliance().orElse(Alliance.Red) == Alliance.Blue) {
+        if(Constants.kAllianceSupplier.get() == Alliance.Blue) {
             swerve.resetOdometry(path.getStartingDifferentialPose());
         }
         else {
