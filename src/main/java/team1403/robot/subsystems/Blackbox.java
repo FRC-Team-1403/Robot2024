2f12cf8c081aa0d207565eaff8bdb6a39dea008b
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2024-10-19 11:40:20 -0400

    remove auto finished from blackbox

--- src/main/java/team1403/robot/subsystems/Blackbox.java
+++ src/main/java/team1403/robot/subsystems/Blackbox.java
@@ -7,8 +7,7 @@ import team1403.robot.Constants.Setpoints;
 
 public class Blackbox {
     //expand further later to include arm and wrist angles along with RPMs 
-    private static boolean trigger;
-    private static boolean auto_finished;
+    private static boolean trigger = false;
     public static Pose2d targetPosition = null;
     //requested setpoint must have intake speed of 0!
     public static SonicBlasterSetpoint requestedSetpoint = Setpoints.kDriveSetpoint;
@@ -17,16 +16,8 @@ public class Blackbox {
         return trigger;
     }
 
-    public static boolean getAutoFinished() {
-        return auto_finished;
-    }
-
     public static void setTrigger(boolean trig) {
         trigger = trig;
-    }
-
-    public static void setAutoFinished(boolean finished) {
-        auto_finished = finished;
     }
 
     public static boolean isValidTargetPosition() {
