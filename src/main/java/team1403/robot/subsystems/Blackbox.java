50334e32e3250984ce6ae6f8ab21444d582a2f04
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2024-10-24 20:47:36 -0400

    add Blackbox.isLoaded

--- src/main/java/team1403/robot/subsystems/Blackbox.java
+++ src/main/java/team1403/robot/subsystems/Blackbox.java
@@ -4,10 +4,12 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import team1403.robot.Constants.Setpoints;
+import team1403.robot.commands.TriggerShotCommand;
 
 public class Blackbox {
     //expand further later to include arm and wrist angles along with RPMs 
     private static boolean trigger = false;
+    private static boolean loaded = false;
     public static Pose2d targetPosition = null;
     //requested setpoint must have intake speed of 0!
     public static SonicBlasterSetpoint requestedSetpoint = Setpoints.kDriveSetpoint;
@@ -18,6 +20,18 @@ public class Blackbox {
 
     public static void setTrigger(boolean trig) {
         trigger = trig;
+    }
+
+    public static Command shoot() {
+        return new TriggerShotCommand();
+    }
+
+    public static void setLoaded(boolean l) {
+        loaded = l;
+    }
+
+    public static boolean isLoaded() {
+        return true;
     }
 
     public static boolean isValidTargetPosition() {
