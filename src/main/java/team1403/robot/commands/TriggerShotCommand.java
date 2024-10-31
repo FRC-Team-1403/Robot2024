a44ed7ff6d4b24536771e6ab537f1321a437fa1a
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2024-10-30 20:08:29 -0400

    TriggerShotCommand Timeout

--- src/main/java/team1403/robot/commands/TriggerShotCommand.java
+++ src/main/java/team1403/robot/commands/TriggerShotCommand.java
@@ -1,17 +1,23 @@
 package team1403.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import team1403.robot.subsystems.Blackbox;
 
 public class TriggerShotCommand extends Command {
     
+    Timer timer = new Timer();
+
     @Override
     public void initialize() {
         Blackbox.setTrigger(true);
+        timer.restart();
     }
 
     @Override
     public boolean isFinished() {
+        if(timer.hasElapsed(2)) Blackbox.setTrigger(false);
+        
         return !Blackbox.getTrigger();
     }
 
